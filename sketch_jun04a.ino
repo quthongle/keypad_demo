d0a619427e87e9ea477604c5ca1984c0f8f35025
quthonglee <quthonglee@yahoo.com>
2016-06-04 09:39:02 +0700

    quick edit...for TOM

--- sketch_jun04a.ino
+++ sketch_jun04a.ino
@@ -1,23 +1,22 @@
 
 
 #include "Keypad.h"
- 
+
 
 // keypad type definition
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
 char keys[ROWS][COLS] =
-  {{'1','2','3'},
-    {'4','5','6'},
-    {'7','8','9'},
-    {'*','0','#'}};
+		{{'1','2','3'},
+				{'4','5','6'},
+				{'7','8','9'},
+				{'*','0','#'}};
 
 byte rowPins[ROWS] = {
-    5, 4, 3, 2}; //connect to the row pinouts of the keypad :: pin 5-6-7 on keypad's pin
+				5, 4, 3, 2}; //connect to the row pinouts of the keypad :: pin 5-6-7 on keypad's pin
 byte colPins[COLS] = {
-    8, 7, 6}; // connect to the column pinouts of the keypad :: pin 4-3-2-1 on keypad's pin
+				8, 7, 6}; // connect to the column pinouts of the keypad :: pin 4-3-2-1 on keypad's pin
 
-int count=0;
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
@@ -25,22 +24,17 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 //---------------------------
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("...testing keypad...");
+		Serial.begin(9600);
+		Serial.println("...testing keypad...");
 }
 
 //-----------------------------
 void loop()
 {
-    char key = keypad.getKey();
-    if (key != NO_KEY)
-    {
-        Serial.println(key);
-        count++;
-        if (count==17)
-        {
-            Serial.println(key);
-            count=0;
-        }
-    }
+				char key = keypad.getKey();
+				if (key != NO_KEY)
+				{
+								Serial.println(key);
+								Serial.println(key);
+				}
 }
